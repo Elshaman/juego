5ce5908122daf0f5d49a88e56a43ab66c749f92b
shaman <cbo@misena.edu.co>
2018-07-18 21:02:22 -0500

    juego hasta generacion de aliens

---
+++ settings.py
@@ -0,0 +1,29 @@
+class Settings():
+
+    def __init__(self):
+
+        #settings de balas
+        self.bullet_speed_factor = 4
+        self.bullet_width = 4
+        self.bullet_height = 25
+        self.bullet_color = (60,60,60)
+        self.bullets_allowed = 4
+
+        #settings de pantalla
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
+
+        #settings de nave
+        self.ship_speed_factor = 3.0
+
+        #settings de alien
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
+
+
+
+
+        
---
+++ ship.py
@@ -0,0 +1,37 @@
+import pygame
+
+
+class Ship():
+
+    def __init__(self , ai_settings, screen):
+        self.screen = screen
+
+        self.ai_settings = ai_settings
+
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+        self.center = float(self.rect.centerx)
+
+
+
+        self.moving_right = False
+        self.moving_left = False
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
+
+    def update(self):
+        #Si se mueve a la derecha y no alcanza el limite derecho
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        # Si se mueve a la izquierda y no alcanza el limite izquierdo
+        elif self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed_factor
+
+
+        self.rect.centerx = self.center
